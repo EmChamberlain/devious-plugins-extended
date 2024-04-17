53531f61887e49a691fcbe7ddbb5a0f6ed95288c
emily <emilydatascience@gmail.com>
2024-04-17 12:10:50 -0500

    Updated logout to optionally check for skulls and also use tp

--- unethical-logout/src/main/java/net/unethicalite/plugins/logout/LogoutPlugin.java
+++ unethical-logout/src/main/java/net/unethicalite/plugins/logout/LogoutPlugin.java
@@ -11,6 +11,7 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.unethicalite.api.commons.Time;
 import net.unethicalite.api.entities.Players;
 import net.unethicalite.api.game.Game;
 import net.unethicalite.api.items.Equipment;
@@ -46,7 +47,7 @@ public class LogoutPlugin extends Plugin
 			return;
 
 		int wildyLevel = Game.getWildyLevel();
-		if (wildyLevel < 1)
+		if (wildyLevel < 1 && !config.test())
 		{
 			return;
 		}
@@ -66,13 +67,23 @@ public class LogoutPlugin extends Plugin
 
 			String gloryTeleAction = "Edgeville";
 			Item teleItem = Equipment.getFirst(x -> x.hasAction(gloryTeleAction));
-
 			String teleAction = gloryTeleAction;
+
+			int maxWildyLevel = 30;
 
 			if (teleItem == null)
 			{
 				log.info("No equipped glory");
-				teleItem = Inventory.getFirst(x -> x.getName().toLowerCase().contains("amulet of glory("));
+				var inventoryGlory = Inventory.getFirst(x -> x.getName().toLowerCase().contains("amulet of glory("));
+				if (inventoryGlory != null && inventoryGlory.hasAction("Wear"))
+				{
+					log.info("Trying to wear glory");
+					inventoryGlory.interact("Wear");
+					boolean timedOut = !Time.sleepUntil(() -> Equipment.contains(x -> x.hasAction(gloryTeleAction)), 2000);
+					if (timedOut) log.info("Timed out for equipping glory");
+					teleItem = Equipment.getFirst(x -> x.hasAction(gloryTeleAction));
+					teleAction = gloryTeleAction;
+				}
 			}
 
 			if (teleItem == null)
@@ -81,9 +92,10 @@ public class LogoutPlugin extends Plugin
 				String tabletTeleAction = "Break";
 				teleItem = Inventory.getFirst(x -> x.hasAction(tabletTeleAction));
 				teleAction = tabletTeleAction;
+				maxWildyLevel = 20;
 			}
 
-			if (config.teleportOut() && teleItem != null)
+			if (config.teleportOut() && teleItem != null && wildyLevel <= maxWildyLevel)
 			{
 				log.info("Trying to tele");
 				try {
