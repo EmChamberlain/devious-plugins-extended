46022374312abd9ad07c975ecf341e5ad8ed5c63
emily <emilydatascience@gmail.com>
2024-05-10 22:49:06 -0500

    Added fang as valid dragon farming weapon

--- unethical-dragonfarmer/src/main/java/net/unethicalite/plugins/dragonfarmer/DragonFarmerPlugin.java
+++ unethical-dragonfarmer/src/main/java/net/unethicalite/plugins/dragonfarmer/DragonFarmerPlugin.java
@@ -141,7 +141,8 @@ public class DragonFarmerPlugin extends LoopedPlugin
                 Item dagger = Inventory.getFirst(x -> x.getName().toLowerCase().contains("dragon dagger"));
                 Item whip = Inventory.getFirst(x -> x.getName().toLowerCase().contains("whip"));
                 Item hasta = Inventory.getFirst(x -> x.getName().toLowerCase().contains("hasta"));
-                List<Integer> validIds = List.of(cape == null ? -1 : cape.getId(), antifire == null ? -1 : antifire.getId(), dagger == null ? -1 : dagger.getId(), whip == null ? -1 : whip.getId(), hasta == null ? -1 : hasta.getId());
+                Item fang = Inventory.getFirst(x -> x.getName().toLowerCase().contains("fang"));
+                List<Integer> validIds = List.of(cape == null ? -1 : cape.getId(), antifire == null ? -1 : antifire.getId(), dagger == null ? -1 : dagger.getId(), whip == null ? -1 : whip.getId(), hasta == null ? -1 : hasta.getId(), fang == null ? -1 : fang.getId());
                 Item itemToDeposit = Inventory.getFirst(x -> !validIds.contains(x.getId()));
 
                 if (itemToDeposit != null)
